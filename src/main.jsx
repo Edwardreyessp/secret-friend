392cb4a19bebbd8bab8cc3195a240acb77819c48
Edwardreyessp <edwardreyessp@gmail.com>
2022-11-12 23:22:50 -0600

    refactor: Routes url

--- src/main.jsx
+++ src/main.jsx
@@ -3,39 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import SecretFriend from "./routes/SecretFriend";
 import "./index.scss";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "friend/:id",
-        element: <SecretFriend />,
-      },
-    ],
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    {/* <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Router path="/" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/friend/:id" element={<SecretFriend />} />
+        <Route path="*" element={<SecretFriend to="/friend/:id" replace />} />
       </Routes>
-    </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
